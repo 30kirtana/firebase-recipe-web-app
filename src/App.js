43687cf5e1d32150e4efb4e90af07975a5dda103
K.Kirtana <2005381@kiit.ac.in>
2025-06-16 15:49:07 +0530

    added some code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
+import LoginForm from "./components/LoginForm";
+
 // eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
-// eslint-disable-next-line no-unused-vars
-import firebase from './FirebaseConfig';
+// // eslint-disable-next-line no-unused-vars
+// import firebase from './FirebaseConfig';
 
 function App() {
+  const [ user, setUser ] = useState(null);
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+  
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
+      <LoginForm existingUser={ user }></LoginForm>
       </div>
     </div>
   );
